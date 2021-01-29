991413149bf429a2c2adf779901f1e35e0baf00b
Bayan Bokan <beckbokanov@gmail.com>
2021-01-29 18:04:48 +0600

    сложное задание сделал

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+let num = 266219;
+let arrOfNums = num.toString().split('');
+let result;
+
+for (let i = 0; i < arrOfNums.length; i++) {
+  result =
+    +arrOfNums[i] *
+    +arrOfNums[i + 1] *
+    +arrOfNums[i + 2] *
+    +arrOfNums[i + 3] *
+    +arrOfNums[i + 4] *
+    +arrOfNums[i + 5];
+  break;
+}
+console.log('Произведение цифр этого числа равно ' + result);
+
+result = result ** 3;
+alert('Результат равен ' + result);
+alert('Первые две цифры равны ' + result.toString().substring(0, 2));
